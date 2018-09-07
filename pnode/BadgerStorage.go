a8fa1b375a4d8214c43a3ab9ec6be2a9e1ac3ea0
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-09-07 13:58:28 -0500

    WIP

---
+++ pnode/BadgerStorage.go
@@ -0,0 +1,16 @@
+package pnode
+
+
+// https://github.com/dgraph-io/badger
+/*
+import (
+    "path"
+    "os"
+
+    "github.com/plan-tools/go-plan/pdi"
+    "github.com/plan-tools/go-plan/plan"
+
+    "github.com/dgraph-io/badger"
+)
+
+*/
