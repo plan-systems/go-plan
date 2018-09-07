e2fc64563c85d69ef1ec936821cb3e8faba53769
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-09-07 13:59:26 -0500

    WIP

---
+++ pnode/StorageProviders/storage_test.go
@@ -0,0 +1,26 @@
+package main
+
+
+import (
+
+    "github.com/plan-tools/go-plan/pnode/StorageProviders/bolt"
+
+    //"fmt"
+
+    "testing"
+)
+
+
+
+func TestCommunityEncryption(t *testing.T) {
+
+   
+	{
+    //t.Fatal("woot")
+    }
+
+
+}
+
+
+
