b994bf97bc7ff0f58deb313d0fc76098508568d1
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-09-07 02:54:09 -0500

    WIP

--- pnode/BoltStorage.go
+++ pnode/BoltStorage.go
@@ -3,13 +3,12 @@ package pnode
 // See: go-plan/pdi/StorageProvider.go for interface context
 
 import (
-	"bytes"
-	"encoding/base64"
-    "encoding/binary"
-	"os"
-	"path"
-	"time"
-	"sync"
+    "bytes"
+    "encoding/base64"
+    "os"
+    "path"
+    "time"
+    "sync"
     "sync/atomic"
 
 	"github.com/plan-tools/go-plan/pdi"
@@ -67,7 +66,7 @@ func (storage *BoltStorage) StartSession(
 		msgsOut:                  inMsgChannel,
         txnBatchInbox:            make(chan *txnBatch, 8),
 		dbPathname:               path.Join(storage.dbsPathname, string(dbName)+".bolt"),
-		maxTxnReportsBeforePause: 20,
+		maxTxnReportsBeforePause: 10,
 		maxTxnBytesBeforePause:   1000000,
         //alertOutbox:              make([]pdi.StorageAlert, 0, 10),
 	}
@@ -136,7 +135,7 @@ type txnBatch struct {
 	requestID     pdi.RequestID
 	txnsRequested []pdi.TxnRequest
 
-	txnsToCommit  []*pdi.StorageTxn // nil if n/a
+	txnsToCommit  []pdi.StorageTxn // nil if n/a
 }
 
 
@@ -156,8 +155,12 @@ type boltSession struct {
     txnBatchPool    sync.Pool
 
 	txnBatchInbox    chan *txnBatch
-
     nextRequestID  uint32
+
+    readheadPos  pdi.TimeSortableKey
+    readerReqID  pdi.RequestID
+	readerEvent    chan int32
+
 
 	//txnBatchQueue txnBatchQueue
 }
@@ -174,26 +177,364 @@ func (session *boltSession) readerWriter() {
     }
 
     for {
+        //var batch *txnBatch
 
-        // TODO: pol 
+        // FIRST, choose requests made via RequestTxns() and CommitTxns(), THEN make incremental progress frorm the read head.
+        select {
+            case batch := <- session.txnBatchInbox:
 
-        batch := <- session.txnBatchInbox
+                // Sending a nil batch means we're shutting down
+                if batch == nil {
+                    return
+                }
 
-        // If nil is sent, it means it's time to exit
-        if batch == nil {
-            return
+                session.doTxn(batch)
+
+                // We're done with this item, so make it available for reuse
+                session.txnBatchPool.Put(batch)
+
+            default:
+
+                // When the readhead is catchin up to the current membnt
+                sleepMS := <-session.readerEvent
+
+                if ( sleepMS >= 0 ) {
+                    time.Sleep(time.Millisecond * time.Duration(sleepMS))
+                }
+
+                session.doTxn(nil)
         }
 
-        session.commitBatch(batch)
+        
 
-        session.reportBatch(batch)
+        // If nil is sent, it means it's time to exit
+    
 
-        // We're done with this item, so make it available for reuse
-        session.txnBatchPool.Put(batch)
     }
 
 }
+
+
+
+
+func (session *boltSession) readStep(tx *bolt.Tx, bucket *bolt.Bucket) []pdi.StorageTxn {
+
+    txns := make([]pdi.StorageTxn, session.maxTxnReportsBeforePause)
+        
+    bytesProcessed := 0
+
+    c := bucket.Cursor()
+    var err error
+    count := 0
+
+    for k, txnBuf := c.Seek(session.readheadPos[:]); k != nil && count < len(txns); k, txnBuf = c.Next() {
+
+        bytesProcessed += len(txnBuf)
+
+        err = txns[count].UnmarshalWithOptionalBody(txnBuf, true)
+        if err != nil {
+            err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn.Unmarhal() failed")
+        } else {
+            if txn.TimeCommitted != req.TimeCommitted || bytes.Compare(txn.TxnName, req.TxnName) != 0 {
+                err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn verificiation failed")
+            }
+        }
+
+        copy(session.readheadPos[:], k[:])
+        session.readheadPos.Incrment()
+
+        count++
+        if bytesProcessed > session.maxTxnBytesBeforePause {
+            break;
+        }
+
+    }
+
+    txns = txns[:txnCount]
+
+
+    return nil
+}
+
+
+func (session *boltSession) readTxns(tx *bolt.Tx, bucket *bolt.Bucket, txnsRequested []pdi.TxnRequest) []pdi.StorageTxn {
+
+    N := len(txnsRequested)
+    txns := make([]pdi.StorageTxn, N)
+
+    {
+        for i, req := range txnsRequested {
+
+            txn := &txns[i]
+
+            // Lookup the txn (speedy since its keyed by time)
+            timeKey := pdi.FormTimeSortableKey(req.TimeCommitted, req.TxnName)
+            txnBuf := bucket.Get(timeKey[:])
+            if txnBuf == nil {
+                txn.TxnName       = req.TxnName
+                txn.TimeCommitted = req.TimeCommitted
+                txn.TxnStatus     = pdi.TxnStatus_INVALID_TXN
+
+                // TODO: add log msg?
+                continue
+            }
+
+            err := txn.UnmarshalWithOptionalBody(txnBuf, req.IncludeBody)
+            if err != nil {
+                err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn.Unmarhal() failed")
+            } else {
+                if txn.TimeCommitted != req.TimeCommitted || bytes.Compare(txn.TxnName, req.TxnName) != 0 {
+                    err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn verificiation failed")
+                }
+            }
+
+            if err != nil {
+                txn.TxnName       = req.TxnName
+                txn.TimeCommitted = req.TimeCommitted
+                txn.TxnStatus     = pdi.TxnStatus_LOST
+
+                // TODO: add log msg?
+                continue
+            }
+        }
+    }
+
+    return txns
+}
+
+
+
+func (session *boltSession) commitTxns(tx *bolt.Tx, bucket *bolt.Bucket, txns []pdi.StorageTxn) error {
+
+    N := len(txns)
+    var txnBuf []byte
+
+    curTime := plan.Now().UnixSecs
+
+    var err error
+
+    for i := 0; i < N; i++ {
+
+        txn := &txns[i]
+
+        txn.TimeCommitted = curTime
+        txn.TimeConsensus = curTime 
+
+        // In a local db like this, once the entry is written, it won't ever revert (and we need to set this before marshalling)
+        txn.TxnStatus = pdi.TxnStatus_FINALIZED
+        
+        txnBuf, err = txn.MarshalForOptionalBody(txnBuf)
+
+        if err == nil {
+            timeKey := pdi.FormTimeSortableKey(txn.TimeCommitted, txn.TxnName)
+            err = bucket.Put(timeKey[:], txnBuf)
+        }
+
+        // Regardless of if the commit success, nil out each txn body so the StorageTxns sent back don't include the body
+        txn.Body = nil
+
+        // If a txn errors, now (and the below will be called)
+        if err != nil {
+            return err
+        }
+    }
+
+    return err
+}
+
+
+
+
+
+func (session *boltSession) doTxn(
+    txnOp *txnBatch,
+    ) {
+
+    msg := pdi.StorageMsg{
+        RequestID: txnOp.requestID,
+    }
+
+
+    if txnOp == nil {
+        msg.StorageOp = pdi.OpTxnReport
+    } else {
+        txnsToCommit  := len(txnOp.txnsToCommit)
+        txnsRequested := len(txnOp.txnsRequested)
+        plan.Assert(txnsToCommit == 0 || txnsRequested == 0, "txnBatch assert failed")
+        
+        if txnsToCommit > 0 {
+            msg.Txns = txnOp.txnsToCommit
+            msg.StorageOp = pdi.OpCommitTxns
+        } else if txnsRequested > 0 {
+            msg.StorageOp = pdi.OpRequestTxns
+        }
+    }
+
+    tx, dbErr := session.db.Begin(msg.StorageOp == pdi.OpCommitTxns)
+    var bucket *bolt.Bucket
+    if dbErr == nil {
+		bucket = tx.Bucket(storageTxnBucketName)
+        if bucket == nil {
+            dbErr = plan.Errorf(dbErr, plan.FailedToCommitTxn, "storage bucket '%s' not found", storageTxnBucketName)
+        }
+    }
+
+    if dbErr == nil {
+        switch msg.StorageOp {
+            case pdi.OpCommitTxns:
+                dbErr = session.commitTxns(tx, bucket, txnOp.txnsToCommit)
+            case pdi.OpRequestTxns:
+                msg.Txns = session.readTxns(tx, bucket,txnOp.txnsRequested)
+            case pdi.OpTxnReport:
+                msg.Txns = session.readStep(tx, bucket)
+
+        }
+
+        // If we encountered an error, rollback the db (commit none) -- otherwise, commit.
+        if dbErr != nil {
+            tx.Rollback()
+        } else {
+            dbErr = tx.Commit()
+            
+            if dbErr != nil {
+                dbErr = plan.Error(dbErr, plan.FailedToCommitTxn, "bolt.Tx.Commit() failed")
+
+                // If the commmit failed, update the status only for commit msgs
+                if dbErr != nil {
+                    switch msg.StorageOp {
+                        case pdi.OpCommitTxns:
+                            msg.AlertCode = pdi.CommitFailed
+
+                            // If we get an error for any of the sub txns, fail them all
+                            for _, txn := range msg.Txns {
+                                txn.Body = nil
+                                txn.TxnStatus = pdi.TxnStatus_FAILED_TO_COMMIT
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+
+    if dbErr != nil {
+        if msg.AlertCode == 0 {
+            msg.AlertCode = pdi.StorageFailure
+        }
+        if len(msg.AlertMsg) == 0 {
+            msg.AlertMsg = dbErr.Error()
+        }
+    }
+
+    session.msgsOut <- msg
+}
+
 /*
+func (session *boltSession) readBurst() {
+
+    // Create the return msg
+    msg := pdi.StorageMsg{
+        RequestID: readerReqID,
+        Txns: make([]*StorageTxn, session.maxTxnReportsBeforePause),
+    }
+    
+    tx, err := session.db.Begin(false)
+    var bucket *bolt.Bucket
+    if err == nil {
+		bucket = tx.Bucket(storageTxnBucketName)
+        if bucket == nil {
+            err = plan.Errorf(err, plan.FailedToCommitTxn, "storage bucket '%s' not found", storageTxnBucketName)
+        }
+    }
+
+    if err != nil {
+        msg.AlertMsg  = err.Error()
+        msg.AlertCode = pdi.StorageFailure
+        msg.Txns      = nil
+    } else {
+    
+        for i, req:= range inBatch.txnsRequested {
+
+            txn := &pdi.StorageTxn{}
+            msg.Txns[i] = txn
+
+            // Lookup the txn (speedy since its keyed by time)
+            timeKey := pdi.FormTimeSortableKey(req.TimeCommitted, req.TxnName)
+            txnBuf := bucket.Get(timeKey[:])
+            if txnBuf == nil {
+                txn.TxnName       = req.TxnName
+                txn.TimeCommitted = req.TimeCommitted
+                txn.TxnStatus     = pdi.TxnStatus_INVALID_TXN
+
+                // TODO: add log msg?
+                continue
+            }
+
+            err = txn.UnmarshalWithOptionalBody(txnBuf, req.IncludeBody)
+            if err != nil {
+                err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn.Unmarhal() failed")
+            } else {
+                if txn.TimeCommitted != req.TimeCommitted || bytes.Compare(txn.TxnName, req.TxnName) != 0 {
+                    err = plan.Error(err, plan.FailedToUnmarshalTxn, "StorageTxn verificiation failed")
+                }
+            }
+
+            if err != nil {
+                txn.TxnName       = req.TxnName
+                txn.TimeCommitted = req.TimeCommitted
+                txn.TxnStatus     = pdi.TxnStatus_LOST
+
+                // TODO: add log msg?
+                continue
+            }
+        }
+
+// **close DB!
+
+
+        // If one of the txns failed, don't cause an alert
+        err = nil
+    }
+
+    // Send the result message back to the client session
+    session.msgsOut <- msg
+
+
+}
+*/
+
+/*
+
+	t, err := db.Begin(false)
+	if err != nil {
+		return err
+	}
+
+	// Make sure the transaction rolls back in the event of a panic.
+	defer func() {
+		if t.db != nil {
+			t.rollback()
+		}
+	}()
+
+	// Mark as a managed tx so that the inner function cannot manually rollback.
+	t.managed = true
+
+	// If an error is returned from the function then pass it through.
+	err = fn(t)
+	t.managed = false
+	if err != nil {
+		_ = t.Rollback()
+		return err
+	}
+
+	if err := t.Rollback(); err != nil {
+		return err
+	}
+
+	return nil
+
+
 
     N := len(inTxnNames)
     session.txnReqsMux.Lock()
@@ -241,7 +582,8 @@ unc (db *DB) Update(fn func(*Tx) error) error {
                 log.Println(alert)
 
                 return alert
-*/
+
+
 
 
 
@@ -271,33 +613,7 @@ func (session *boltSession) commitBatch(inBatch *txnBatch) {
         msg.AlertCode = pdi.StorageFailure
         msg.Txns      = nil
     } else {
- 
-        var txnBuf []byte
-
-	    curTime := plan.Now().UnixSecs
-
-        for i, txn := range inBatch.txnsToCommit {
-
-			txn.TimeCommitted = curTime
-            txn.TimeConsensus = curTime 
-
-			// In a local db like this, once the entry is written, it won't ever revert (and we need to set this before marshalling)
-			txn.TxnStatus = pdi.TxnStatus_FINALIZED
-            
-            txnBuf, err = txn.MarshalForOptionalBody(txnBuf)
-
-			if err == nil {
-                timeKey := marshalTimeSortableKey(txn.TimeCommitted, txn.TxnName)
-                err = bucket.Put(timeKey[:], txnBuf)
-            }
-
-            // If a txn errors, stop where we're at rollback
-			if err != nil {
-                break
-            }
-        }
-
-
+        msg.Txns, dbErr = commitTxns(tx, bucket, msg.Txns)
     }
 
     if tx != nil {
@@ -339,7 +655,7 @@ func (session *boltSession) commitBatch(inBatch *txnBatch) {
 
 
 
-func (session *boltSession) reportBatch(inBatch *txnBatch) {
+func (session *boltSession) readBatch(inBatch *txnBatch) {
 
     if len(inBatch.txnsRequested) == 0 {
         return
@@ -372,7 +688,7 @@ func (session *boltSession) reportBatch(inBatch *txnBatch) {
             msg.Txns[i] = txn
 
             // Lookup the txn (speedy since its keyed by time)
-            timeKey := marshalTimeSortableKey(req.TimeCommitted, req.TxnName)
+            timeKey := pdi.FormTimeSortableKey(req.TimeCommitted, req.TxnName)
             txnBuf := bucket.Get(timeKey[:])
             if txnBuf == nil {
                 txn.TxnName       = req.TxnName
@@ -414,14 +730,14 @@ func (session *boltSession) reportBatch(inBatch *txnBatch) {
 }
 
 
-
+*/
 
 func (session *boltSession) startNewRequest() *txnBatch {
 
     batch := session.txnBatchPool.Get().(*txnBatch)
     batch.txnsRequested = batch.txnsRequested[:0]
     batch.txnsToCommit  = batch.txnsToCommit[:0]
-    batch.requestID     = pdi.RequestID(atomic.AddUint32( &session.nextRequestID, 1))
+    batch.requestID     = pdi.RequestID(atomic.AddUint32(&session.nextRequestID, 1))
     
     return batch
 }
@@ -448,7 +764,7 @@ func (session *boltSession) RequestTxns(
 
 
 func (session *boltSession) CommitTxns(
-    inTxns []*pdi.StorageTxn,
+    inTxns []pdi.StorageTxn,
     ) (pdi.RequestID, error) {
 
     if session.status != pdi.SessionIsReady {
@@ -622,20 +938,3 @@ func (q *txnBatchQueue) Pop() *txnBatch {
 }
 */
 
-
-
-
-func marshalTimeSortableKey(inTime int64, inTxnName []byte) [32]byte {
-    var k [32]byte
-
-    binary.BigEndian.PutUint64(k[0:8], uint64(inTime))
-
-	overhang := pdi.StorageTxnNameSz - len(inTxnName)
-	if overhang < 0 {
-        copy(k[8:], inTxnName[-overhang:])
-	} else {
-        copy(k[8+overhang:], inTxnName)
-    }
-
-	return k
-}
