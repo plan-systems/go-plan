41d138be3ad81f1ab83cf8814cb56108000b80a8
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-09-13 02:43:54 -0500

    arch edits

--- pdi/StorageProvider.go
+++ pdi/StorageProvider.go
@@ -19,8 +19,7 @@ type StorageProvider interface {
 	StartSession(
 		inDatabaseID []byte,
 		inMsgChannel chan<- StorageMsg,
-		inOnCompletion func(StorageSession, error),
-	) error
+	) (StorageSession, error)
 }
 
 /*****************************************************
@@ -28,7 +27,7 @@ type StorageProvider interface {
 **/
 
 // StorageSession wraps a persistent storage (or replicating) service "consumer"
-// All calls in this interface are implemented as threadsafe.
+// All calls to this interface are implemented to be THREADSAFE.
 type StorageSession interface {
 
 	// IsReady reports if this session is open and ready to receive requests
@@ -40,11 +39,12 @@ type StorageSession interface {
 	// RequestFromBookmark sets the session's "read head position" based on state information returned via GetBookmark() from this or a previous session.
 	RequestFromBookmark(inFromBookmark plan.Block) (RequestID, error)
 
-	// GetBookmark returns an opaque, StorageProvider-specifc blob of state information that a client uses for StartReporting().
+	// GetBookmark returns an opaque, StorageProvider-specific blob of state information that a client uses for StartReporting().
 	GetBookmark() (*plan.Block, error)
 
-	// CommitTxns submits the given finished entry to the storage implementation for publishing.
-	CommitTxns(inTxns []*StorageTxn) (RequestID, error)
+	// CommitTxn submits the given block to the storage implementation for publishing.  Once the inTxnBody has been committed,
+	//    a corresponding StorageTxn will appear in the session's StorageMsg channel with status.
+	CommitTxn(inTxnBody *plan.Block) (RequestID, error)
 
 	// EndSession ends this session, resulting in the sessions parent provider signal the session's end.
 	// Following a call to EndSession(), no more references to this session should be made -- StorageProvider.StartSession() must be called again.
@@ -52,10 +52,11 @@ type StorageSession interface {
 }
 
 // RequestID allows a StorageSession client to identify/match StorageReports as they arrive from the StorageProvider.
+// A valid RequestID is always >= 0
 type RequestID uint32
 
 // TxnRequest requests a given txn by name and commit time (both are required)
-// If LoadBody == false, then StorageTxn.TxnBody will be nil (saving bandwidth)
+// If IncludeBody == false, then StorageTxn.TxnBody will be nil (saving bandwidth)
 type TxnRequest struct {
 	TxnName       []byte
 	TimeCommitted int64
@@ -66,15 +67,31 @@ type TxnRequest struct {
 ** StorageMsg
 **/
 
-// StorageMsg serves two purposes:
+// StorageMsg serves two top-level purposes:
 //    (a) transport requested StorageTxns (and their status) to StorageSession clients
 //    (b) sending important storage alerts to storage clients (e.g. txn failures or system shutdown warnings)
 type StorageMsg struct {
-	RequestID RequestID     // Set to 0 if n/a
-	Txns      []*StorageTxn // READ-ONLY for clients once committed.  These txns have finished processing (successfully or unsuccessfully)
-	AlertCode AlertCode     // Set to 0 if n/a or no alert given
-	AlertMsg  string        // Human-readable amplifying information
+	RequestID RequestID // Set to 0 if n/a
+	StorageOp StorageOp
+	Txns      []StorageTxn // READ-ONLY for clients once committed.  These txns have finished processing (successfully or unsuccessfully)
+	AlertCode AlertCode    // Set to 0 if n/a or no alert given
+	AlertMsg  string       // Human-readable amplifying information
 }
+
+// StorageOp describes what kind of storage operation is/was happening
+type StorageOp int32
+
+const (
+
+	// OpCommitTxns means the given txn are/were to be committed (and this StorageMsg is reporting status)
+	OpCommitTxns = 1 + iota
+
+	// OpRequestTxns means the given txns are/were requested
+	OpRequestTxns
+
+	// OpTxnReport means this the given txns have been updated or are the next batch of txns resulting from RequestFromBookmark()
+	OpTxnReport
+)
 
 func (alert *StorageMsg) Error() string {
 	return fmt.Sprintf("%s {code:%d}", alert.AlertMsg, alert.AlertCode)
@@ -85,11 +102,38 @@ type AlertCode int32
 
 const (
 
-	// SessionIsReady means this StorageSession is ready for requests
-	SessionIsReady AlertCode = 5900 + iota
+	/*****************************************************
+	** SessionInfoAlerts
+	**/
 
-	// SessionWillEnd means this StorageSession will end soon, described in StorageEvent.Info
+	// SessionInfoAlertMask is used to filter alerts that are informative and don't represent direct concern
+	SessionInfoAlertMask AlertCode = 0x0100 + iota
+
+	// SessionIsReady means this StorageSession is ready for requests
+	SessionIsReady
+
+	// SessionWillEnd means this StorageSession will end soon, described in StorageMsg.AlertMsg
 	SessionWillEnd
+
+	/*****************************************************
+	** SessionFailureAlerts
+	**/
+
+	// SessionFailureAlertMask is used to filter alerts that reflect a falure or problem happening
+	SessionFailureAlertMask AlertCode = 0x0200 + iota
+
+	// StorageFailure means database access failed in an unexpected way
+	StorageFailure
+
+	// CommitFailed means the given CommitTxns() request failed
+	CommitFailed
+
+	/*****************************************************
+	** SessionEndedAlerts
+	**/
+
+	// SessionEndedAlertMask is used to filter alerts that means the given StorageSession has ended (and is not ready)
+	SessionEndedAlertMask AlertCode = 0x0400 + iota
 
 	// SessionEndedByClient means this StorageSession was ended via StorageSession.EndSession()
 	SessionEndedByClient
@@ -99,10 +143,4 @@ const (
 
 	// SessionErroredOut means this StorageSession ended due to an error
 	SessionErroredOut
-
-	// StorageFailure means database access failed in an unexpected way
-	StorageFailure
-
-	// CommitFailed means the given CommitTxns() request failed
-	CommitFailed
 )
