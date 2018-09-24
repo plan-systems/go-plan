d35abaede272a78b66fec37610f884da37d3ee11
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-09-24 14:35:39 -0500

    now tests all SKI providers

--- ski/Providers/providers_test.go
+++ ski/Providers/providers_test.go
@@ -1,31 +1,65 @@
-package ski // import "github.com/plan-tools/go-plan/ski"
+package main
 
 import (
 	"bytes"
     "math/rand"
     "fmt"
+    //"log"
 
     "testing"
 
+	"github.com/plan-tools/go-plan/ski"
 	"github.com/plan-tools/go-plan/plan"
+
+
+	"github.com/plan-tools/go-plan/ski/Providers/nacl"
+
 )
 
 
 
 
+var gTesting *testing.T
 
 
 func TestCommunityEncryption(t *testing.T) {
 
-    A := newSession(t, "Alice")
-    B := newSession(t, "Bob")
+    gTesting = t
+
+    providersToTest := []string {}
+
+    ski.RegisterProvider(nacl.Provider)
+
+    // Register providers to test 
+    providersToTest = append(providersToTest, 
+        nacl.Provider.InvocationStr(),
+    )
+
+    for _, invocationStr := range providersToTest {
+
+        invocation := plan.Block{
+            Label: invocationStr,
+        }
+
+        A := newSession(invocation, "Alice")
+        B := newSession(invocation, "Bob")
+
+        doCoreTests(A, B)
+
+        A.endSession("done A")
+        B.endSession("done B")
+    }
+}
+
+
+func doCoreTests(A, B *testSession) {
 
     // 1) make a new community key
-	opResults, err := A.doOp(OpArgs{
-        OpName: OpCreateCommunityKey,
+	opResults, err := A.doOp(ski.OpArgs{
+        OpName: ski.OpCreateCommunityKey,
     })
 	if err != nil {
-		t.Fatal(err)
+		gTesting.Fatal(err)
     }
     communityKeyID := plan.GetKeyID(opResults.Content)
 
@@ -33,53 +67,53 @@ func TestCommunityEncryption(t *testing.T) {
     fmt.Printf("%s's encryptPubKey %v\n", B.name, B.encryptPubKey)
 
     // 2) generate a xfer community key msg from A
-    opResults, err = A.doOp(OpArgs{
-        OpName: OpSendCommunityKeys,
+    opResults, err = A.doOp(ski.OpArgs{
+        OpName: ski.OpSendCommunityKeys,
         OpKeyIDs: []plan.KeyID{communityKeyID},
         PeerPubKey: B.encryptPubKey,
         CryptoKeyID: A.encryptPubKeyID,
     })
     if err != nil {
-        t.Fatal(err)
+        gTesting.Fatal(err)
     }
     
     // 3) insert the new community key into B
-    opResults, err = B.doOp(OpArgs{
-        OpName: OpAcceptCommunityKeys,
+    opResults, err = B.doOp(ski.OpArgs{
+        OpName: ski.OpAcceptCommunityKeys,
         Msg: opResults.Content,
         PeerPubKey: A.encryptPubKey,
         CryptoKeyID: B.encryptPubKeyID,
     })
     if err != nil {
-        t.Fatal(err)
+        gTesting.Fatal(err)
     }
 
 	clearMsg := []byte("hello, PLAN community!")
 
     // Encrypt a new community msg on A
-	opResults, err = A.doOp(OpArgs{
-        OpName: OpEncryptForCommunity,
+	opResults, err = A.doOp(ski.OpArgs{
+        OpName: ski.OpEncryptForCommunity,
         CryptoKeyID: communityKeyID,
         Msg: clearMsg,
     })
 	if err != nil {
-		t.Fatal(err)
+		gTesting.Fatal(err)
 	}
 
     encryptedMsg := opResults.Content
 
     // Send the encrypted community message to B
-	opResults, err = B.doOp(OpArgs{
-        OpName: OpDecryptFromCommunity,
+	opResults, err = B.doOp(ski.OpArgs{
+        OpName: ski.OpDecryptFromCommunity,
         CryptoKeyID: communityKeyID,
         Msg: encryptedMsg,
     })
 	if err != nil {
-		t.Fatal(err)
+		gTesting.Fatal(err)
     }
 
 	if ! bytes.Equal(clearMsg, opResults.Content) {
-		t.Fatalf("expected %v, got %v after decryption", clearMsg, opResults.Content)
+		gTesting.Fatalf("expected %v, got %v after decryption", clearMsg, opResults.Content)
     }
     
 
@@ -93,13 +127,13 @@ func TestCommunityEncryption(t *testing.T) {
         copy(badMsg, encryptedMsg)
         badMsg[rndPos] += rndAdj
 
-        _, err = B.doOp(OpArgs{
-            OpName: OpDecryptFromCommunity,
+        _, err = B.doOp(ski.OpArgs{
+            OpName: ski.OpDecryptFromCommunity,
             CryptoKeyID: communityKeyID,
             Msg: badMsg,
         })
         if err == nil {
-            t.Fatal("there should have been a decryption error!")
+            gTesting.Fatal("there should have been a decryption error!")
         }
     }
     
@@ -119,7 +153,7 @@ func TestCommunityEncryption(t *testing.T) {
 
 type testSession struct {
     name        string
-    session     Session
+    session     ski.Session
     blocker     chan int   
     signingKeyID plan.KeyID
 
@@ -130,7 +164,7 @@ type testSession struct {
 
 
 
-func (ts *testSession) doOp(inOpArgs OpArgs) (*plan.Block, *plan.Perror) {
+func (ts *testSession) doOp(inOpArgs ski.OpArgs) (*plan.Block, *plan.Perror) {
 
     var outErr *plan.Perror
     var outResults *plan.Block
@@ -150,41 +184,54 @@ func (ts *testSession) doOp(inOpArgs OpArgs) (*plan.Block, *plan.Perror) {
 
 
 
+func (ts *testSession) endSession(inReason string) {
+
+    ts.session.EndSession(inReason, func(inParam interface{}, inErr *plan.Perror) {
+        if inErr != nil {
+            gTesting.Fatal(inErr)
+        }
+        ts.blocker <- 1
+    })
+
+    <- ts.blocker
+
+}
+
+
 
 // test setup helper
-func newSession(t *testing.T, inName string) *testSession {
+func newSession(inInvocation plan.Block, inName string) *testSession {
 
     ts := &testSession{
         name:inName,
         blocker:make(chan int, 100),
     }
 
-
-    NaclProvider.StartSession(
-        NaClProviderName,
-        GatewayRWAccess,
-        func(inSession Session, inErr *plan.Perror) {
-
-            ts.session = inSession
-            ts.blocker <- 1
-        },
+    var err *plan.Perror
+    ts.session, err = ski.StartSession(
+        inInvocation,
+        ski.GatewayRWAccess,
         nil,
     )
+    if err != nil {
+        gTesting.Fatal(err)
+    }
 
-    <- ts.blocker
     identityResults, err := ts.doOp(
-        OpArgs{
-            OpName: OpNewIdentityRev,
+        ski.OpArgs{
+            OpName: ski.OpNewIdentityRev,
         })
     if err != nil {
-        t.Fatal(err)
+        gTesting.Fatal(err)
     }
 
 
-    ts.signingKeyID = plan.GetKeyID( identityResults.GetContentWithLabel(PubSigningKeyName) )
+    ts.signingKeyID = plan.GetKeyID( identityResults.GetContentWithLabel(ski.PubSigningKeyName) )
 
-    ts.encryptPubKey = identityResults.GetContentWithLabel(PubCryptoKeyName)
+    ts.encryptPubKey = identityResults.GetContentWithLabel(ski.PubCryptoKeyName)
     ts.encryptPubKeyID = plan.GetKeyID( ts.encryptPubKey )
 
     return ts
 }
+
+
