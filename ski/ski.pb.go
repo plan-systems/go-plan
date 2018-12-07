459da049d109154222c55892cb978c54f8e21977
Drew O'Meara <andrew.omeara@soundspectrum.com>
2018-12-07 16:22:49 -0600

    updated plan-protobufs URL

--- ski/ski.pb.go
+++ ski/ski.pb.go
@@ -1,3 +1,3 @@
 
-// If you're reading this, see https://github.com/plan-tools/plan-protobufs
-package YOU_NEED_TO_BUILD_PROTOBUFS
+// If you're reading this, see https://github.com/plan-systems/plan-protobufs
+package YOU_MUST_BUILD_PLAN_PROTOBUFS
