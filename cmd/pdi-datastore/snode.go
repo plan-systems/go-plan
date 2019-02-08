2604f439def583bbe62e6b1b200b154336dda4ee
Drew O'Meara <drew2019@plan-systems.org>
2019-02-08 12:53:37 -0600

    removed cruft

--- cmd/pdi-datastore/snode.go
+++ cmd/pdi-datastore/snode.go
@@ -11,15 +11,11 @@ import (
     "sync"
     //"time"
     "net"
+    "crypto/rand"
     //"encoding/hex"
     "encoding/json"
-    "encoding/base64"
 
     log "github.com/sirupsen/logrus"
-
-
-
-    //"github.com/tidwall/redcon"
 
     //"github.com/plan-systems/go-plan/ski"
     "github.com/plan-systems/go-plan/plan"
@@ -27,30 +23,13 @@ import (
     "github.com/plan-systems/go-plan/pservice"
 
     ds "github.com/plan-systems/go-plan/pdi/StorageProviders/datastore"
-    //github.com/plan-systems/go-plan/ski"
-    _ "github.com/plan-systems/go-plan/ski/CryptoKits/nacl"
+    _ "github.com/plan-systems/go-plan/ski/CryptoKits/nacl"    
 
-    //"github.com/plan-systems/go-plan/ski/Providers/nacl"
-
-    // This inits in with sql, so no named import is needed
-    _ "github.com/mattn/go-sqlite3"
-    
-    //"crypto/md5"
-    //"hash"
-    "crypto/rand"
-
-    //"github.com/stretchr/testify/assert"
     "github.com/ethereum/go-ethereum/common/hexutil"
 
-
-
-    //"golang.org/x/net/context"
+    "golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-
-    //"github.com/ethereum/go-ethereum/rlp"
-
-    "golang.org/x/net/context"
 
 )
 
@@ -90,17 +69,7 @@ type Snode struct {
     grpcServer                  *grpc.Server
     listener                    net.Listener
     grpcDone                    chan struct{}
-    ShuttingDown                chan struct{}
-
-    FSNameEncoding              *base64.Encoding
 }
-
-
-// RuntimeSettings specifies settings that are solely associated with system load, performance, and resource allocation.
-type RuntimeSettings struct {
-
-}
-
 
 
 
@@ -112,8 +81,6 @@ type Config struct {
     NodeID                      hexutil.Bytes                   `json:"node_id"`
 
     StorageConfigs              []ds.StorageConfig              `json:"storage_configs"`
-
-    RuntimeSettings             RuntimeSettings                 `json:"runtime_settings"`
 
     DefaultFileMode             os.FileMode                     `json:"default_file_mode"`
 
@@ -150,24 +117,6 @@ func (config *Config) ReadFromFile(inPathname string) error {
     return err
 }
 
-/*
-func (config *Config) ReadFromFile(inPathname string) *plan.Perr {
-    buf, err := ioutil.ReadFile(inPathname)
-    if err != nil {
-        return plan.Errorf(err, plan.ConfigNotRead, "error reading Snode config file %v", inPathname)
-    }
-
-    err = json.Unmarshal(buf, config)
-    if err != nil {
-        return plan.Errorf(err, plan.ConfigNotRead, "error unmarshalling Snode config file %v", inPathname)
-    }
-
-    return err
-}
-
-*/
-
-
 // WriteToFile attempts to save node config info to the given pathname.
 func (config *Config) WriteToFile(inPathname string) error {
 
@@ -182,14 +131,9 @@ func (config *Config) WriteToFile(inPathname string) error {
 }
 
 
-
-
-
 // NewSnode creates and initializes a new Snode instance
 func NewSnode(inBasePath *string) (*Snode, error) {
     sn := &Snode{
-        FSNameEncoding: base64.RawURLEncoding,
-        ShuttingDown: make(chan struct{}),
         grpcDone: make(chan struct{}),
         Stores: make(map[plan.CommunityID]*ds.Store),
     }
@@ -217,7 +161,6 @@ func NewSnode(inBasePath *string) (*Snode, error) {
 
     return sn, nil
 }
-
 
 // ReadConfig uses sn.BasePath to read in the node config file
 func (sn *Snode) ReadConfig(inInit bool) error {
@@ -262,7 +205,6 @@ func (sn *Snode) CreateNewStore(
     inEpoch pdi.StorageEpoch,
 ) error {
     
-
     stConfig := &ds.StorageConfig{
         HomePath: path.Join("datastore", plan.MakeFSFriendly(inEpoch.CommunityName, inEpoch.CommunityID[:2])),
         ImplName: inImplName,
@@ -346,7 +288,6 @@ func (sn *Snode) Startup() error {
 }
 
 
-
 func (sn *Snode) registerStore(St *ds.Store) {
 
     communityID := plan.GetCommunityID(St.Epoch.CommunityID)
@@ -357,7 +298,6 @@ func (sn *Snode) registerStore(St *ds.Store) {
     sn.Stores[communityID] = St
 
 }
-
 
 // StartServer is used like main()
 func (sn *Snode) StartServer() error {
@@ -390,7 +330,6 @@ func (sn *Snode) StartServer() error {
     return nil
 }
 
-
 // Shutdown performs a shutdown and cleanup
 func (sn *Snode) Shutdown() {
     log.Info("Shutting down...")
@@ -414,81 +353,8 @@ func (sn *Snode) Shutdown() {
 
 }
 
-
-
-
-
-
-/*
-
-var storageMsgPool = sync.Pool{
-    New: func() interface{} {
-        return new(StorageMsg)
-    },
-}
-
-// RecycleStorageMsg effectively deallocates the item and makes it available for reuse
-func RecycleStorageMsg(inMsg *StorageMsg) {
-    for _, txn := range inMsg.Txns {
-        txn.Body = nil  // TODO: recycle plan.Blocks too
-    }
-    storageMsgPool.Put(inMsg)
-}
-
-// NewStorageMsg allocates a new StorageMsg
-func NewStorageMsg() *StorageMsg {
-
-    msg := storageMsgPool.Get().(*StorageMsg)
-    if msg == nil {
-        msg = &StorageMsg{}
-    } else {
-        msg.Txns = msg.Txns[:0]
-        msg.AlertCode = 0
-        msg.AlertMsg = ""
-    }
-
-    return msg
-}
-
-// NewStorageAlert creates a new storage msg with the given alert params
-func NewStorageAlert(
-    inAlertCode AlertCode, 
-    inAlertMsg string,
-    ) *StorageMsg {
-
-    msg := NewStorageMsg()
-    msg.AlertCode = inAlertCode
-    msg.AlertMsg = inAlertMsg
-
-    return msg 
-
-}
-
-
-
-// SnodeID identifies a particular Snode running as part of a PLAN community
-type SnodeID                [32]byte
-
-func (pnID SnodeID) MarshalJSON() ( []byte, error ) {
-    bytesNeeded := base64.RawURLEncoding.EncodedLen( len(pnID) ) + 2
-    outText := make( []byte, bytesNeeded, bytesNeeded )
-    outText[0] = '"'
-    base64.RawURLEncoding.Encode( outText[1:bytesNeeded-1], pnID[:] )
-    outText[bytesNeeded-1] = '"'
-
-    return outText, nil
-}
-
-func (pnID SnodeID) UnmarshalJSON( inText []byte ) error {
-    _, err := base64.RawURLEncoding.Decode( pnID[:], inText )
-
-    return err
-}
-*/
-
-
 // StartSession -- see service StorageProvider in pdi.proto
-func (sn *Snode) StartSession(ctx context.Context, in *pservice.SessionRequest) (*pdi.StorageSession, error) {
+func (sn *Snode) StartSession(ctx context.Context, in *pservice.SessionRequest) (*pdi.ProviderInfo, error) {
     cID := plan.GetCommunityID(in.CommunityId)
 
     St := sn.Stores[cID]
@@ -504,23 +370,8 @@ func (sn *Snode) StartSession(ctx context.Context, in *pservice.SessionRequest) 
     // TODO security checks to prevent DoS
     session :=  sn.ActiveSessions.NewSession(ctx)
     session.Cookie = St
-    /*
-    go func() {
-        for i := 0; i < 10; i++ {
-            tryDesc := fmt.Sprintf("try-%v", i)
-            log.Print("sending: ", tryDesc)
 
-            inOutlet.Send(&pservice.Msg{
-                Label: tryDesc,
-            })
-
-            time.Sleep(5 * time.Second)   
-        }
-    }()
-
-    time.Sleep(60 * time.Second)   */
-
-    msg := &pdi.StorageSession{
+    msg := &pdi.ProviderInfo{
         TxnEncoderInvocation: St.TxnDecoder.TxnEncoderInvocation(),
     }
 	return msg, nil
@@ -571,125 +422,3 @@ func (sn *Snode) CommitTxn(inTxn *pdi.ReadiedTxn, inOutlet pdi.StorageProvider_C
     err = <-job.OnComplete
     return err
 }
-
-
-/*
-func (sn *Snode) InitOnDisk(initWithStorageType string) error {
-
-    sn.Config.SnodeID = make([]byte, plan.CommunityIDSz)
-    rand.Read(sn.Config.SnodeID)
-
-    var datastore *ds.Datastore
-    var err error
-    
-    switch sn.Config.ImplName {
-        case "badger":
-            datastore, err = badger.NewDatastore(path, nil)
-    }
-
-    sn.Config.ImplName = initWithStorageType
-
-    return sn.WriteConfigOut()
-}
-
-
-func (sn *Snode) LoadConfigIn() error {
-
-    buf, err := ioutil.ReadFile(sn.GetConfigPathname())
-    if err != nil {
-        return err
-    }
-
-    err = json.Unmarshal(buf, &sn.Config)
-    if err != nil {
-        return err
-    }
-
-    for i := range sn.Config.Stores {
-        CSInfo := &sn.Config.Stores[i]
-        
-        CS := NewDatastore(CSInfo, sn)
-
-        sn.RegisterCStore(CS)
-    }
-
-    return err
-}
-*/
-
-
-/*
-
-func (sn *Snode) SetupDirForClient(inClientID []byte) string {
-    clientDir := sn.FSNameEncoding.EncodeToString(inClientID)
-
-    pathname := path.Join(sn.Params.ClientStoragePath, clientDir)
-
-    os.MkdirAll(pathname, sn.Config.DefaultFileMode)
-
-    return pathname
-}
-*/
-
-
-
-
-/*
-
-func (sn *Snode) CreateNewCommunity( inCommunityName string ) *CommunityRepo {
-
-    sn.Config.RepoList = append(sn.Config.RepoList, CommunityRepoInfo{})
-
-    info := &sn.Config.RepoList[len(sn.Config.RepoList)-1]
-    
-    {
-        info.CommunityName = inCommunityName
-        info.CommunityID = make([]byte, plan.CommunityIDSz)
-        rand.Read(info.CommunityID)
-
-        {
-            var b strings.Builder
-            remapCharset := map[rune]rune{
-                ' ':  '-',
-                '.':  '-',
-                '?':  '-',
-                '\\': '+',
-                '/':  '+',
-                '&':  '+',
-            }
-    
-            for _, r := range strings.ToLower( info.CommunityName ) {
-                if replace, ok := remapCharset[r]; ok {
-                    if replace != 0 {
-                        b.WriteRune(replace)
-                    }
-                } else {
-                    b.WriteRune(r)
-                }
-            }
-    
-    
-            info.RepoPath = b.String() + "-" + hex.EncodeToString( info.CommunityID[:4] ) + "/"
-            info.TimeCreated = plan.Now()
-            info.MaxPeerClockDelta = 60 * 25
-        }
-
-    }
-
-    CR := NewCommunityRepo(info, pn)
-
-    sn.RegisterCommunityRepo(CR)
-
-    sn.WriteConfigOut()
-
-    return CR
-
-}
-
-
-
-*/
-
-
-
-
