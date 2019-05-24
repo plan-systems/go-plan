3f1fb56aace7150140f0348226b6b338a0a44adb
Drew O'Meara <drew2019@plan-systems.org>
2019-05-24 14:19:12 -0500

    comment edits

--- ski/ski.go
+++ ski/ski.go
@@ -14,6 +14,11 @@ import (
 // FUTURE: this will become a gRPC service.
 type Session interface {
 
+    // Merges all keys in the given KeyTome with this host KeyTome.
+    // See docs for KeyTome.MergeTome() on how error conditions are addressed.
+    // Note: incoming duplicate key entries are ignored/dropped.
+    //ImportKeys(srcTome *KeyTome) error
+
     // Generates a new KeyEntry for each entry in srcTome (based on the entry's KeyType and CryptoKitID, ignoring the rest) and merges it
     //    with the host KeyTome. A copy of each newly generated entry (except for PrivKey) is placed into result KeyTome.
     // See "KeyGen mode" notes where KeyEntry is declared. 
