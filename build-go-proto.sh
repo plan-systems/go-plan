b41837f0a86d4195307de2e6f6b3fde3e8602830
Tim Gross <tim@0x74696d.com>
2019-07-01 16:31:44 -0400

    made protobuf build scripts portable
    - fixed interpreter directive to match bashism in use
    - sed: made in-place edit portable between BSD and GNU sed
    - sed: reduced escaping complexity by using ~ as delimiter
    - shellcheck: mostly quoting fixes
    - added halt-on-error directive

--- build-go-proto.sh
+++ build-go-proto.sh
@@ -1,13 +1,13 @@
-
-#!/bin/sh
+#!/bin/bash
 #
 # See:
-#    plan-systems/plan-protobuf/README.md 
+#    plan-systems/plan-protobuf/README.md
 #    http://plan-systems.org
 #
 #
+set -e
 
-SELF=`basename "$0"`
+SELF=$(basename "$0")
 
 if [[ $# -ne 2 ]]; then
     echo "Usage: ./build-go-proto.sh <plan_pkg_name> <out_path>"
@@ -18,17 +18,17 @@ PKG_NAME="$1"
 DST_DIR="$2"
 
 BUILD_PROTO="../plan-protobufs/build-proto.sh"
-
-replace='s/#PKG# \"#PKG#\"/#PKG# \"github.com\/plan-systems\/plan-core\/#PKG#\" \/\/\/ Redirected by '$SELF' :)/' 
-
-#echo "$replace"
-#echo "${replace//#PKG#/plan}"
-
 $BUILD_PROTO "$PKG_NAME" gofast "$DST_DIR"
-sed -i ''	-e "${replace//#PKG#/plan}" 	\
-			-e "${replace//#PKG#/ski}"		\
-			-e "${replace//#PKG#/client}"   \
-			-e "${replace//#PKG#/pdi}"		\
-			-e "${replace//#PKG#/repo}" 	\
-			"$DST_DIR/$PKG_NAME/$PKG_NAME.pb.go"
 
+# we need the canonical go import path, so edit the generated file
+replace='s~#PKG# "#PKG#"~#PKG# "github.com/plan-systems/plan-core" /// Redirected by '$SELF' :)~'
+
+echo "$DST_DIR/$PKG_NAME/$PKG_NAME.pb.go"
+
+sed -i'' \
+    -e "${replace//#PKG#/plan}" 	\
+    -e "${replace//#PKG#/ski}"		\
+    -e "${replace//#PKG#/client}"   \
+    -e "${replace//#PKG#/pdi}"		\
+    -e "${replace//#PKG#/repo}" 	\
+    "$DST_DIR/$PKG_NAME/$PKG_NAME.pb.go"
