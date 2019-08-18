b74a5286b839c3218b1b84dcd77435c70cb81b56
Drew O'Meara <drew2019@plan-systems.org>
2019-08-18 15:11:41 -0500

    added MemberSeat.SeatDesc()

--- client/client.support.go
+++ client/client.support.go
@@ -1,1 +1,13 @@
 package client
+
+import (
+    "fmt"
+    "github.com/plan-systems/plan-core/plan"
+)
+
+// SeatDesc returns a string containing useful info about this MemberSeat
+func (seat *MemberSeat) SeatDesc() string {
+    commID := plan.BinEncode(seat.GenesisSeed.CommunityEpoch.CommunityID)
+    return fmt.Sprintf("seat:%s, member:%v(%d), community:%v", 
+        plan.BinEncode(seat.SeatID), seat.MemberEpoch.Alias, seat.MemberEpoch.MemberID, commID)
+}
