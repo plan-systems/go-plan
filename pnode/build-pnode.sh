83944cbbf06148421533870e1dd81d59e0ae424d
Drew O'Meara <drew2019@plan-systems.org>
2020-10-01 00:17:47 -0500

    handy build script

---
+++ pnode/build-pnode.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# if [[ -d "../redwood-go" ]]; then
+#     echo "=====  updating redwood-go  ====="
+#     git -C ../redwood-go pull
+#     $cmd
+# fi
+
+SELF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# go get -u github.com/gogo/protobuf/protoc-gen-gofast
+# go get -u google.golang.org/grpc
+
+BUILD_GO_PROTO="$SELF_DIR/../../plan-protobufs/build-proto.sh"
+DST_DIR="$SELF_DIR/.."
+
+$BUILD_GO_PROTO repo  gofast "$DST_DIR"
+$BUILD_GO_PROTO vault gofast "$DST_DIR"
+
+
+# echo "=====  generating protobufs  ====="
+# go get -u github.com/golang/protobuf/protoc-gen-go
+# go generate .
+
+echo "=====  building pnoode  ====="
+go build --tags static -o pnode
+
+
+
